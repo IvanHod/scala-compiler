6f8755d180a69dc1f5ff3e9480a0deb3a09f655f
ivan <vandamkakdam1@bk.ru>
2016-04-25 16:46:44 +0300

    Добавил тестирование сразу нескольких файлов

--- main.c
+++ main.c
@@ -4,25 +4,43 @@
 #include "structs.h"
 #include <string.h>
 #include "printTree.h"
+#include <stdbool.h>
 
 #define PATH_TO_DIR "/home/ivan/qt-creator-projects/scala-compiler/"
-#define FILE_NAME "simpleExample.txt"
+#define COUNT_TEST 3
+
+bool isOpenFile(FILE *file, char* name) {
+    if( file != NULL)
+        return true;
+    else {
+        printf("Error: Can`t open file - %s.\n", name);
+        return false;
+    }
+}
 
 int main(int argc, char *argv[])
 {
-    char path_to_file[100] = PATH_TO_DIR;
-    strcat(path_to_file, FILE_NAME);
-    FILE *file = fopen( path_to_file, "r");
-    if( file != NULL ) {
-        yyin = file;
-        int r = yyparse();
-        printf("%d", r);
-    } else
-        printf("error: Can`t open file.");
-    writeFile = fopen("/home/ivan/qt-creator-projects/scala-compiler/outTree.xml", "wb");
-    if( writeFile == NULL)
-        printf("can not open file for write;");
-    printf_root();
-    fclose(writeFile);
+    char files[COUNT_TEST][30] = {
+        "declaration_vars.txt",
+        "arithmetical_operations.txt",
+        "logical_operation.txt"
+    };
+    int i = 0;
+    for( i = 0; i < COUNT_TEST; i++ ) {
+        char file_path[100] = PATH_TO_DIR;
+        strcat(file_path, files[i]);
+        FILE *file = fopen( file_path, "r");
+        if( isOpenFile(file, files[i] ) ) {
+            yyin = file;
+            yyparse();
+            char writeFile_path[100] = PATH_TO_DIR;
+            strcat(writeFile_path, files[i]);
+            writeFile = fopen(writeFile_path, "wb" );
+            if( isOpenFile(writeFile, files[i]) ) {
+                printf_root();
+                fclose(writeFile);
+            }
+        }
+    }
     return 0;
 }
